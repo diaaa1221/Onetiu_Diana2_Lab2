cc05c91363b500ced43f9f82caa71eb17d4565ff
DIANA-MARIA ONEȚIU <diana.onetiu@stud.ubbcluj.ro>
2024-11-01 11:00:45 +0200

    Laborator4

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Onetiu_Diana2_Lab2.Data;
+using Onetiu_Diana2_Lab2.Migrations;
 using Onetiu_Diana2_Lab2.Models;
+using Onetiu_Diana2_Lab2.Models.ViewModels;
 
 namespace Onetiu_Diana2_Lab2.Pages.Categories
 {
@@ -21,9 +24,27 @@ namespace Onetiu_Diana2_Lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+                .ThenInclude(bc => bc.Book)
+                .ThenInclude(b => b.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(bc => bc.Book);
+            }
         }
     }
 }
